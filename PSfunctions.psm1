6f88ec69d40f31d6ac75f853826f950a2aa945c2
Lintegras <lintegras@gmail.com>
2024-09-25 09:16:57 -0700

    Update PSfunctions.psm1

--- PSfunctions.psm1
+++ PSfunctions.psm1
@@ -1,48 +1,47 @@
 function New-MFARequest {
     param (
-        [string]$EmailToPush
+    	[string]$EmailToPush
     )
-	######### Variables #########
-	$ApplicationId = $ENV:ApplicationID
-	$ApplicationSecret = $ENV:ApplicationSecret
-	$TenantId = $ENV:TenantID
-	$ClientId = "981f26a1-7f43-403b-a875-f8b09b8cd720"
-	######### Variables ########
-
-Write-Host "Creating secure credentials and secrets..." -ForegroundColor Green
+    ######### Variables #########
+    $ApplicationId = $ENV:ApplicationID
+    $ApplicationSecret = $ENV:ApplicationSecret
+    $TenantId = $ENV:TenantID
+    $ClientId = "981f26a1-7f43-403b-a875-f8b09b8cd720"
+    ######### Variables ########
+    
+    Write-Host "Creating secure credentials and secrets..." -ForegroundColor Green
     Write-Host "Converting the Client Secret to a Secure String..." -ForegroundColor Green
     $SecureClientSecret = ConvertTo-SecureString -String $ApplicationSecret -AsPlainText -Force
     Write-Host "Done." -ForegroundColor Green
     Write-Host "Creating a PSCredential Object Using the Client ID and Secure Client Secret..." -ForegroundColor Green
     $ClientSecretCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId, $SecureClientSecret
     Write-Host "Done." -ForegroundColor Green
-    Write-Host "Connecting to Microsoft Graph Using the Tenant ID and Client Secret Credential..." -ForegroundColor Green
-	Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $ClientSecretCredential -NoWelcome
+    Write-Host "Connecting to Microsoft Graph Using the Tenant ID and Client Secret Credential..." -ForegroundColor Gree
+    Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $ClientSecretCredential -NoWelcome
     Write-Host "Done." -ForegroundColor Green
     Write-Host "Secure credentials and secrets created, successfully connected to Microsoft Graph." -ForegroundColor Green
     Write-Host "Creating Secure Secret for MFA Token request..." -ForegroundColor Green
-    $ServicePrincipalId = (Get-MgServicePrincipal -Filter "appid eq '$ClientId'").Id
-	$params = @{
-		passwordCredential = @{
-			displayName = "My Application MFA"
-		}
-	}
-	$Secret = (Add-MgServicePrincipalPassword -ServicePrincipalId $ServicePrincipalId -BodyParameter $params).SecretText
+    $ServicePrincipalId = (Get-MgServicePrincipal -Filter "appid eq '$ClientId'").I
+    $params = @{
+    	passwordCredential = @{
+		displayName = "My Application MFA"
+	}	
+    }
+    $Secret = (Add-MgServicePrincipalPassword -ServicePrincipalId $ServicePrincipalId -BodyParameter $params).SecretText
     Sleep 15
-    Write-Host "Done." -ForegroundColor Green
-	Write-Host "Getting MFA Client Access Token..." -ForegroundColor Green
-	$Body = @{
-		'resource'      = "https://adnotifications.windowsazure.com/StrongAuthenticationService.svc/Connector"
-		'client_id'     = $ClientId
-		'client_secret' = $Secret
-		'grant_type'    = "client_credentials"
-		'scope'         = "openid"
-	}
+    Write-Host "Done." -ForegroundColor Green	
+    Write-Host "Getting MFA Client Access Token..." -ForegroundColor Green
+    $Body = @{
+    	'resource'      = "https://adnotifications.windowsazure.com/StrongAuthenticationService.svc/Connector"
+	'client_id'     = $ClientId
+	'client_secret' = $Secret
+	'grant_type'    = "client_credentials"
+	'scope'         = "openid"
+    }
     $mfaClientToken = Invoke-RestMethod -Method post -Uri "https://login.microsoftonline.com/$TenantId/oauth2/token" -Body $Body
     Write-Host "Done." -ForegroundColor Green
-    
-	Write-Host "Generating XML for MFA Push request..." -ForegroundColor Green
-	$XML = @"
+    Write-Host "Generating XML for MFA Push request..." -ForegroundColor Green
+    $XML = @"
 <BeginTwoWayAuthenticationRequest>
 	<Version>1.0</Version>
 	<UserPrincipalName>$EmailToPush</UserPrincipalName>
@@ -61,12 +60,11 @@ Write-Host "Creating secure credentials and secrets..." -ForegroundColor Green
 	<CallerIP>UNKNOWN:</CallerIP>
 </BeginTwoWayAuthenticationRequest>
 "@
-	Write-Host "Done." -ForegroundColor Green
-
-	Write-Host "Generating MFA Request..." -ForegroundColor Green
-	$Headers = @{ "Authorization" = "Bearer $($mfaClientToken.access_token)" }
-	$mfaResult = Invoke-RestMethod -uri 'https://strongauthenticationservice.auth.microsoft.com/StrongAuthenticationService.svc/Connector//BeginTwoWayAuthentication' -Method POST -Headers $Headers -Body $XML -ContentType 'application/xml'
-	Write-Host "Done." -ForegroundColor Green
+    Write-Host "Done." -ForegroundColor Green
+    Write-Host "Generating MFA Request..." -ForegroundColor Green
+    $Headers = @{ "Authorization" = "Bearer $($mfaClientToken.access_token)" }
+    $mfaResult = Invoke-RestMethod -uri 'https://strongauthenticationservice.auth.microsoft.com/StrongAuthenticationService.svc/Connector//BeginTwoWayAuthentication' -Method POST -Headers $Headers -Body $XML -ContentType 'application/xml'
+    Write-Host "Done." -ForegroundColor Green
     
     Disconnect-MgGraph
 	
